2f8476b30710416d9df68c1af5dba4d92b32c243
EdwardHill15 <total-health@kpnmail.nl>
2022-07-22 13:57:41 +0200

    new graphs temp and precipitation Netherlands

--- code/Rscript local weather dot R Scraping weather data from the internet with R and the tidyverse.R
+++ code/Rscript local weather dot R Scraping weather data from the internet with R and the tidyverse.R
@@ -7,7 +7,7 @@
 library(tidyverse)
 library(glue)
 library(lubridate)
-
+library(patchwork)
 
 inventory_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
 
@@ -16,10 +16,7 @@ inventory <- read_table(inventory_url,
 inventory
 
 # Now finding the closest weather station
-
-# Latitude and longitude for Zwaag (Klaproos)
-
-
+# Zwaag, klaproos 23
 
 my_lat <- 52.13 * 2 * pi / 360
 my_lon <- 5.29 * 2 * pi / 360
@@ -47,12 +44,13 @@ my_station <- inventory %>%
          d = 1.609344 * 3963 * acos((sin(lat_r) * sin(my_lat)) + 
                            cos(lat_r) * cos(my_lat) *
                            cos(my_lon - lon_r))) %>% 
-  filter(start < 1960) %>% 
+  filter(start < 1960 & end > 2020) %>% 
   top_n(n = -1, d) %>% 
   distinct(station) %>% 
   pull(station)
 
-# Get and tidy local weather station data (from: "NLE00100501")
+# Get and tidy local weather station data (from: "NLM00006260")
+# KNMI De Bilt: https://www.knmi.nl/over-het-knmi/over
 
 station_daily <- glue("https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/{my_station}.csv.gz")
 
@@ -61,7 +59,7 @@ df <- read_csv(station_daily,
   select(date, variable, value) %>% 
   pivot_wider(names_from = "variable", values_from = "value",
               values_fill = 0) %>% 
-  select(date, TMAX, PRCP)
+  select(date, TMAX, TMIN, PRCP)
 
 # ELEMENT    is the element type.   There are five core elements as well as a number
 # of addition elements.  
@@ -75,22 +73,31 @@ df <- read_csv(station_daily,
 # TMIN = Minimum temperature (tenths of degrees C)
 
 
-df <- df %>% 
+df <- df %>%
   mutate(date = ymd(date))
   
-local_weather <- df  
-tail(local_weather)
-local_weather %>% 
-  mutate(t_max = TMAX / 10) %>% 
-  ggplot(aes(x=date, y=t_max)) +
-  geom_point(size = 4, color = "red") +
-  geom_smooth(method = "lm", formula = y ~x)
+#local_weather <- df  
+# tail(local_weather)
+# local_weather %>% 
+#   mutate(t_max = TMAX / 10) %>% 
+#   ggplot(aes(x=date, y=t_max)) +
+#   geom_point(size = 4, color = "red") +
+#   geom_smooth(method = "lm", formula = y ~x)
 
-t_diff <- local_weather %>% 
-  mutate(year = year(date)) %>% 
-  group_by(year) %>% 
-  summarize(max = max(TMAX / 10)) %>% 
-  ggplot(aes(x=year, y=max, color = max)) +
+df
+NE_max <- aggregate(TMAX~year(date), df, max)
+NE_max$TMAX <- NE_max$TMAX / 10
+colnames(NE_max) <- c("year", "temp")
+NE_max %>% 
+  ggplot(aes(x=year, y=temp, col=temp)) +
+  geom_point(size = 9, alpha = 0.7)
+
+# t_diff <- local_weather %>% 
+#   mutate(year = year(date)) %>% 
+#   group_by(year, TMAX) %>% 
+#   summarize(max = max(TMAX / 10)) %>% 
+p1 <- NE_max %>% 
+  ggplot(aes(x=year, y=temp, color = temp)) +
   geom_point(size = 9, alpha = .7) +
   geom_smooth(method = "lm", formula = y ~x, se = FALSE, color = "gray") +
   scale_color_viridis_c(option = ("magma")) +
@@ -100,15 +107,18 @@ t_diff <- local_weather %>%
 #coord_cartesian(expand=TRUE) +
   scale_y_continuous(breaks = seq(27.5, 37.5, 2.5),
                    limits = c(26, 37.5)) +
+  scale_x_continuous(breaks = seq(1901, 2022, 11),
+                     limits = c(1901, 2023)) +
   labs(y = "Temperature in (\u00B0 C)", 
      x = NULL,
-     title = "Rise temperature in The Netherlands between 1951-2008",
-     subtitle = "Temperature is colored by the size of the max temperature value",
-     caption = "Source: https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt") +
+     title = "Maximum temperature") + 
+     # title = "Maximum Temperature in The Netherlands between 1901 and 2022"
+     # subtitle = "Temperature is colored by the size of the maximum temperature value",
+     # caption = "Source: KNMI weather station De Bilt in The Netherlands (NLM00006260)") +
   theme(
     plot.background = element_rect(fill = "black", color = "black"),
     panel.background = element_rect(fill = "black"),
-    plot.title = element_text(color = "white", face = "bold", size = 25),
+    plot.title = element_text(color = "white", face = "bold", size = 25, hjust = 0.5),
     plot.subtitle = element_text(color = "gray", size =15),
     plot.title.position = "plot",
     plot.caption = element_text(color = "white", size = 12),
@@ -120,10 +130,136 @@ t_diff <- local_weather %>%
     panel.grid.minor.y = element_blank(),
     axis.ticks = element_blank(),
     legend.background = element_rect(fill = "black"),
-    legend.text = element_text(color = "white"),
+    legend.text = element_text(color = "white", size = 6),
     legend.title = element_text(color = "white"),
     legend.position = "bottom")
 library(ggplot2)
-ggsave("rise_temp_max_netherlands.png", height = 6, width = 11)
 
+NE_mean <- aggregate(TMAX~year(date), df, mean)
+NE_mean$TMAX <- NE_mean$TMAX / 10
+colnames(NE_mean) <- c("year", "temp")
+NE_mean$temp <- round(NE_mean$temp, 1)
+library(tidyverse)
+summarize(NE_mean$temp)
+glimpse(NE_mean)
+min(NE_mean$temp)
+max(NE_mean$temp)
+p2 <- NE_mean %>% 
+  ggplot(aes(x=year, y=temp, color = temp)) +
+  geom_point(size = 9, alpha = .7) +
+  geom_smooth(method = "lm", formula = y ~x, se = FALSE, color = "gray") +
+  scale_color_viridis_c(option = ("viridis")) +
+  # scale_color_gradient2(low = "blue", mid = "white", high = "red",
+  #                       midpoint = 13.5) + 
+  #coord_cartesian(xlim=c(10,16)) +
+  #coord_cartesian(expand=TRUE) +
+  scale_y_continuous(breaks = seq(11.5, 16, 1.5),
+                     limits = c(11.5, 16)) +
+  scale_x_continuous(breaks = seq(1901, 2022, 11),
+                     limits = c(1901, 2023)) +
+  labs(y = "Temperature in (\u00B0 C)", 
+       x = NULL,
+       title = "Mean temperature") +
+       # title = "Mean Temperature in The Netherlands between 1901 and 2022"
+       # subtitle = "Temperature is colored by the size of the mean temperature value",
+       # caption = "Source: KNMI weather station De Bilt in The Netherlands (NLM00006260)") +
+  theme(
+    plot.background = element_rect(fill = "black", color = "black"),
+    panel.background = element_rect(fill = "black"),
+    plot.title = element_text(color = "white", face = "bold", size = 25, hjust = 0.5),
+    plot.subtitle = element_text(color = "gray", size =15),
+    plot.title.position = "plot",
+    plot.caption = element_text(color = "white", size = 12),
+    axis.text = element_text(color = "white", size = 15),
+    axis.title = element_text(color = "white", size = 15),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_line(color = "gray", size = 0.25),
+    panel.grid.minor.y = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = "black"),
+    legend.text = element_text(color = "white", size = 6),
+    legend.title = element_text(color = "white"),
+    legend.position = "bottom")
+
+NE_prcp <- aggregate(PRCP~year(date), df, max)
+NE_prcp$PRCP <- NE_prcp$PRCP / 10
+colnames(NE_prcp) <- c("year", "prcp")
+NE_prcp$prcp <- round(NE_prcp$prcp, 1)
+
+min(NE_prcp$prcp)
+max(NE_prcp$prcp)
+summarize(NE_prcp)
+
+NE_prcp <- NE_prcp[-c(1:5),]
+p3 <- NE_prcp %>% 
+  ggplot(aes(x=year, y=prcp, color = prcp)) +
+  geom_point(size = 9, alpha = .7) +
+  geom_smooth(method = "lm", formula = y ~x, se = FALSE, color = "gray") +
+  scale_color_viridis_c(option = ("mako")) +
+  # scale_color_gradient2(low = "blue", mid = "white", high = "red",
+  #                       midpoint = 13.5) + 
+  #coord_cartesian(xlim=c(10,16)) +
+  #coord_cartesian(expand=TRUE) +
+  scale_y_continuous(breaks = seq(15, 65, 10),
+                     limits = c(15, 65)) +
+  scale_x_continuous(breaks = seq(1901, 2022, 11),
+                     limits = c(1901, 2023)) +
+  labs(y = "Precipitation in mm.", 
+       x = NULL,
+       title = "Precipitation") +
+       # title = Precipitation in The Netherlands between 1901 and 2022"
+       # subtitle = "Precipitation is colored by the size of the maximum precipitation in mm. value",
+       # caption = "Source: KNMI weather station De Bilt in The Netherlands (NLM00006260)") +
+  theme(
+    plot.background = element_rect(fill = "black", color = "black"),
+    panel.background = element_rect(fill = "black"),
+    plot.title = element_text(color = "white", face = "bold", size = 25, hjust = 0.5),
+    plot.subtitle = element_text(color = "gray", size =15),
+    plot.title.position = "plot",
+    plot.caption = element_text(color = "white", size = 12),
+    axis.text = element_text(color = "white", size = 15),
+    axis.title = element_text(color = "white", size = 15),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_line(color = "gray", size = 0.25),
+    panel.grid.minor.y = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = "black"),
+    legend.text = element_text(color = "white", size = 6),
+    legend.title = element_text(color = "white"),
+    legend.position = "bottom")
+
+library(patchwork)
+col1 <- (plot_spacer() / p3 /plot_spacer()) +
+  plot_layout(heights = c(0.05, 0.9, 0.05))
+col2 <- (p1 / p2) + plot_layout(heights=c(1,1))
+
+(col1 | col2) + plot_annotation(
+  title = "Temperature and Precipitation in The Netherlands between 1901 and 2022",
+  subtitle = "Temperature and Precipitation is colored by the size of the value",
+  caption = "Source: KNMI weather station De Bilt in The Netherlands (NLM00006260)",
+  tag_levels = c('A', '1'), tag_prefix = 'Fig. ',
+  tag_sep = '.', tag_suffix = ':') &
+  theme(plot.tag = element_text(color = "white"),
+        plot.title = element_text(size = 35),
+        plot.subtitle = element_text(size = 25),
+        plot.caption = element_text(size = 15))
+
+((plot_spacer() / p3 / plot_spacer()) | (p1 / p2)) + plot_annotation(
+  title = "Temperature and Precipitation in The Netherlands between 1901 and 2022",
+  subtitle = "Temperature and Precipitation is colored by the size of the value",
+  caption = "Source: KNMI weather station De Bilt in The Netherlands (NLM00006260)",
+  tag_levels = c('A', '1'), tag_prefix = 'Fig. ',
+                                 tag_sep = '.', tag_suffix = ':') &
+  theme(plot.tag = element_text(color = "white"),
+        plot.title = element_text(size = 35),
+        plot.subtitle = element_text(size = 25),
+        plot.caption = element_text(size = 15)) +
+  plot_layout(heights = unit(c(3,2))
+
+ggsave("rise_temp_max_netherlands.png", height = 6, width = 11)
+ggsave("rise_temp_mean_netherlands.png", height = 6, width = 11)
+ggsave("precipitation_netherlands.png", height = 6, width = 11)
+ggsave("knmi_all.png", height = 10, width = 18)
   
