761bb95a84837c8c95586a84afc88f39be2579d1
EdwardHill15 <total-health@kpnmail.nl>
2022-08-06 16:23:26 +0200

    changes Rscript

--- code/20220608_Rscript_local_weather_ne.R
+++ code/20220608_Rscript_local_weather_ne.R
@@ -6,9 +6,11 @@ pretty_labels <- c("prob_prcp" = "Probality of precipitation",
 
 Sys.setlocale("LC_TIME", "English")
 
+this_year <- year(today())
 today_month <- month(today())
 today_day <- day(today())
-today_date <- ymd(glue("2020-{today_month}-{today_day}"))
+today_date1 <- ymd(glue("2020-{today_month}-{today_day}"))
+today_date <- ymd(glue("2022-{today_month}-{today_day}"))
 
 
 local_weather %>% 
@@ -27,23 +29,24 @@ local_weather %>%
   pivot_longer(cols = c(prob_prcp, mean_prcp, mean_event)) %>% 
   mutate(name = factor(name, levels = c("prob_prcp", "mean_prcp", "mean_event"))) %>% 
   ggplot(aes(x=date, y=value, color = name)) +
-  geom_vline(xintercept = today_date, color = "red", size = 1) +
+  geom_vline(xintercept = today_date1, color = "red", size = 1) +
   geom_line() +
   geom_hline(yintercept = 0, color = "white") +
   geom_smooth(se = FALSE) +
   facet_wrap(~name, ncol = 1, scales = "free_y", 
              strip.position = "left",
              labeller = labeller(name = pretty_labels)) +
-  scale_color_manual(values = c("red", "green", "dodgerblue")) +
+  scale_color_manual(values = c("pink", "green", "dodgerblue")) +
   scale_y_continuous(limits = c(0,NA), expand = c(0,0)) +
   scale_x_date(date_breaks = "2 months", 
                date_labels = "%B") +
   coord_cartesian(clip = "off") +
   labs(x=NULL,
         y=NULL,
-       title = "<span style = 'color: red'>Probability</span> and <span style = 'color: green'>Aver</span><span style = 'color: dodgerblue'>age</span> Precipitation in The Netherlands from 1901 - 2022",
+       title = glue("<span style = 'color: pink'>Probability</span>, <span style = 'color: green'>day average precipitation</span> and <span style = 'color: dodgerblue'>event average precipitation</span>\nin The Netherlands from 1901 - {this_year}"),
        #title = glue("Through {today_month} {today_day}, the precipitation in The Netherlands near De Bilt has a <span style = 'color: red'>probability</span> and <span style = 'color: red'>average</span> for {today_date}")) +
-       subtitle = "The red vertical line is today (August 6th, 2022)") +
+       subtitle = glue("The <span style = 'color: red'>red vertical line</span> is today {today_date}"),
+       caption = "Source data: https://github.com/EdwardHill15/climate_viz") +
   theme(
     panel.background = element_rect(fill = "black"),
     strip.placement = "outside",
@@ -55,6 +58,7 @@ local_weather %>%
     plot.title.position = "plot",
     plot.title = element_textbox_simple(margin=margin(b=10), size = 22, color = "white"),
     plot.subtitle = element_textbox_simple(margin=margin(b=10), size = 15, color = "white"),
+    plot.caption = element_text(color = "white", hjust = 0.9, size = 12),
     axis.title.y = element_text(color = "white"),
     axis.text = element_text(color = "white", size = 12),
     legend.background = element_rect(fill = "black"),
--- code/20220729_Rscript dagwaarden neerslagstations KNMI De Bilt.R
+++ code/20220729_Rscript dagwaarden neerslagstations KNMI De Bilt.R
@@ -56,10 +56,10 @@ describe(df_snow)
 
 # Sneeuwval in Nederland van 1897 tot 20 juni 2022
 
-df_snow
 
+df_prcp <- df[-3]
 df_snow <- df[-2]
-
+df_snow
 df_snow$snow[df_snow$snow %in% c(996:999)] <- 1
 
 describe(df_snow)
@@ -105,5 +105,117 @@ df_snow %>%
 ggsave("figures/cumulative_prcp_ne_black2.png", width = 6, height = 5)
 
 ggsave("figures/cumulative_prcp_ne_black_snow.png", width = 6, height = 5)
+
+
+df_prcp$date <- as.character(df_prcp$date)
+df_prcp$date <- ymd(df_prcp$date)
+today_date <- ymd(today())
+
+this_year <- year(local_weather$date)
+today_month <- month(today())
+today_day <- day(today())
+today_date <- ymd(glue("2020-{today_month}-{today_day}"))
+
+
+
+
+
+df_prcp %>% 
+  mutate(day = day(date),
+         month = month(date),
+         year = year(date)) %>% 
+  drop_na(prcp) %>% 
+  group_by(month, day) %>% 
+  summarize(prob_prcp = mean(prcp > 0),
+            mean_prcp = mean(prcp),
+            mean_event = mean(prcp[prcp > 0]),
+            .groups = "drop") %>% 
+  mutate(date = ymd(glue("2020-{month}-{day}"))) %>% 
+  select(-month, -day) %>% 
+  pivot_longer(cols = c(prob_prcp, mean_prcp, mean_event)) %>% 
+  mutate(name = factor(name, levels = c("prob_prcp", "mean_prcp", "mean_event"))) %>% 
+ggplot(aes(x=date, y=value, color = name)) +
+  geom_vline(xintercept = today_date, color = "red", size = 1) +
+  geom_line() +
+  geom_hline(yintercept = 0, color = "white") +
+  geom_smooth(se = FALSE) +
+  facet_wrap(~name, ncol = 1, scales = "free_y", 
+             strip.position = "left",
+             labeller = labeller(name = pretty_labels)) +
+  scale_color_manual(values = c("red", "green", "dodgerblue")) +
+  scale_y_continuous(limits = c(0,NA), expand = c(0,0)) +
+  scale_x_date(date_breaks = "2 months", 
+               date_labels = "%B") +
+  coord_cartesian(clip = "off") +
+  labs(x=NULL,
+       y=NULL,
+       title = glue("<span style = 'color: red'>Probability</span> and <span style = 'color: green'>Aver</span><span style = 'color: dodgerblue'>age</span> Precipitation in The Netherlands from 1901 - {this_year}"),
+       #title = glue("Through {today_month} {today_day}, the precipitation in The Netherlands near De Bilt has a <span style = 'color: red'>probability</span> and <span style = 'color: red'>average</span> for {today_date}")) +
+       subtitle = glue("The red vertical line is today {today_date}")) +
+  theme(
+    panel.background = element_rect(fill = "black"),
+    strip.placement = "outside",
+    strip.background = element_blank(),
+    strip.text = element_text(color = "white", size = 12), 
+    panel.grid = element_blank(),
+    plot.background = element_rect(fill = "black", color = "black"),
+    axis.line = element_line(),
+    plot.title.position = "plot",
+    plot.title = element_textbox_simple(margin=margin(b=10), size = 22, color = "white"),
+    plot.subtitle = element_textbox_simple(margin=margin(b=10), size = 15, color = "white"),
+    axis.title.y = element_text(color = "white"),
+    axis.text = element_text(color = "white", size = 12),
+    legend.background = element_rect(fill = "black"),
+    # legend.text = element_text(color = "white", size = 10),
+    # legend.title = element_text(color = "white"),
+    #legend.position = "bottom",
+    legend.position = "none",
+    #legend.key = element_rect(fill = "black")
+  )
+  
+  
+  
+  
+  
+  
+  
+  
+  mutate(year = year(date),
+         month = month(date),
+         day = day(date)) %>% 
+  filter(!(month == 2 & day == 29)) %>% 
+  group_by(year) %>% 
+  mutate(cum_snow = cumsum(snow)) %>%
+  ungroup() %>% 
+  ggplot(aes(x=year, y=cum_snow, group = year, col = cum_snow)) +
+  geom_line(size = 4) +
+  scale_x_continuous(breaks = seq(1955, 2022, 5),
+                     labels = seq(1955, 2022, 5),
+                     limits = c(1948, 2022)) +
+  scale_color_gradient2(low = "darkred", mid = "White", high = "red",
+                        midpoint = 50) +
+  scale_y_continuous(breaks = seq(0, 500, 100),
+                     labels = seq(0, 500, 100),
+                     limits = c(0,500),
+                     expand = c(0,0)) +
+  labs(x = NULL,
+       y = "Cumulative snow (cm)",
+       title = glue("Through June 20th, the <span style = 'color: red'>cumulative</span> <span style = 'color: lightblue'>snow</span> in The Netherlands near De Bilt to {this_year}")) +
+  theme(
+    plot.title.position = "plot",
+    plot.title = element_textbox_simple(margin=margin(b=10), size = 16, color = "white"),
+    axis.title.y = element_text(color = "white"),
+    panel.background = element_rect(fill = "black"),
+    axis.line = element_line(),
+    panel.grid = element_blank(),
+    plot.background = element_rect(fill = "black", color = "black"),
+    axis.text = element_text(color = "white", size = 8),
+    legend.background = element_rect(fill = "black"),
+    legend.text = element_text(color = "white", size = 6),
+    legend.title = element_text(color = "white"),
+    legend.position = "bottom")
+
+
+
 
 
